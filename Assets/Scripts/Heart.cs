9c300c73e4c48039175f828167aa01ccd145fa81
Kylin14570 <2813378364@qq.com>
2020-12-15 15:26:01 +0800

    EnemyCorrected

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -8,25 +8,32 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Heart : MonoBehaviour {
-
-    private SpriteRenderer sr;
-    public GameObject explosionPrefab;
-    public AudioClip dieAudio;
-
-    public Sprite BrokenSprite;
+          
+    public Sprite BrokenSprite;         //heart图片
+    private SpriteRenderer sr;          //heart图片的控制变量
+    public GameObject explosionPrefab;  //爆炸特效
+    public AudioClip dieAudio;          //死亡音效
 
 	// Use this for initialization
 	void Start () 
     {
+        //初始化， 获取heart对象
         sr = GetComponent<SpriteRenderer>();
 	}
 
-
+    //死亡方法
     public void Die()
     {
+        //heart图片改为破碎模式
         sr.sprite = BrokenSprite;
+
+        //产生爆炸特效
         Instantiate(explosionPrefab, transform.position, transform.rotation);
-        PlayerManager.Instance.isDefeat = true;
+
+        //产生死亡音效
         AudioSource.PlayClipAtPoint(dieAudio, transform.position);
+
+        //heart被毁，游戏失败
+        PlayerManager.Instance.isDefeat = true;
     }
 }
